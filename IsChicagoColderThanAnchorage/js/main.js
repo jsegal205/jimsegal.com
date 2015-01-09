a63aa192f78630f6e418a1e6d0515d1bb3ab4bac
jsegal205 <jsegal205@gmail.com>
2015-01-08 18:47:48 -0600

    added IsChicagoColderThanAnchorage

---
+++ IsChicagoColderThanAnchorage/js/main.js
@@ -0,0 +1,37 @@
+(function($){
+	var $content = $('#content'),
+		$chicago = $('#chiTemp'),
+		$anch = $('#anchTemp');
+
+	function getChicagoTemp(){
+		$.ajax({
+			type:'POST',
+			url:'https://api.forecast.io/forecast/a895a7c5256b7eadc3074f3485db9406/41.8369,-87.6847',
+			dataType: 'jsonp',
+			success:function(data){
+				$chicago.text(data.currently.temperature);
+				getAnchTemp();
+			}
+		});
+	}
+	
+	function getAnchTemp(){
+		$.ajax({
+			type:'POST',
+			url:'https://api.forecast.io/forecast/a895a7c5256b7eadc3074f3485db9406/61.2175,-149.8584',
+			dataType: 'jsonp',
+			success:function(data){
+				$anch.text(data.currently.temperature);
+				compare();
+			}
+		});
+	}
+	
+	function compare(){
+		var isColder = ($anch.text() > $chicago.text()) ? 'YES' : 'NO';
+		$content.text(isColder);
+	}
+
+	getChicagoTemp();
+
+})(jQuery);
