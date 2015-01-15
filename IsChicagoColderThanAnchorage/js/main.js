2e82aeb9192226116d0002694de0740610a6db94
Jim Segal Macbook <jsegal205@gmail.com>
2015-01-14 22:22:08 -0600

    updated isChicagoColderThanAnchorage/Main.js
    see isChicagoColderThanAnchorage repo for detailed breakdown of changes.

--- IsChicagoColderThanAnchorage/js/main.js
+++ IsChicagoColderThanAnchorage/js/main.js
@@ -1,37 +1,37 @@
 (function($){
-	var $content = $('#content'),
-		$chicago = $('#chiTemp'),
-		$anch = $('#anchTemp');
+	var chicago = '',
+		anch = ''
+		url = 'https://api.forecast.io/forecast/a895a7c5256b7eadc3074f3485db9406/';
 
 	function getChicagoTemp(){
-		$.ajax({
+		return $.ajax({
 			type:'POST',
-			url:'https://api.forecast.io/forecast/a895a7c5256b7eadc3074f3485db9406/41.8369,-87.6847',
+			url: url + '41.8369,-87.6847',
 			dataType: 'jsonp',
 			success:function(data){
-				$chicago.text(data.currently.temperature);
-				getAnchTemp();
+				chicago = data.currently.temperature;
+				$('#chiTemp').text(chicago);
 			}
 		});
 	}
 	
 	function getAnchTemp(){
-		$.ajax({
+		return $.ajax({
 			type:'POST',
-			url:'https://api.forecast.io/forecast/a895a7c5256b7eadc3074f3485db9406/61.2175,-149.8584',
+			url: url + '61.2175,-149.8584',
 			dataType: 'jsonp',
 			success:function(data){
-				$anch.text(data.currently.temperature);
-				compare();
+				anch = data.currently.temperature;
+				$('#anchTemp').text(anc);
 			}
 		});
 	}
 	
 	function compare(){
-		var isColder = (parseInt($anch.text()) > parseInt($chicago.text())) ? 'YES' : 'NO';
-		$content.text(isColder);
+		var isColder = (parseInt(anch) > parseInt(chicago)) ? 'YES' : 'NO';
+		$('#content').text(isColder);
 	}
 
-	getChicagoTemp();
+	getChicagoTemp().then(getAnchTemp).then(compare);
 
 })(jQuery);
