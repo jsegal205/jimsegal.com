d3d5a39cab77b74899d8e2759a53a5b14efc9db4
Jim Segal <jsegal205@gmail.com>
2018-05-17 08:43:53 -0500

    add loading and error text

--- IsChicagoColderThanAnchorage/js/main.js
+++ IsChicagoColderThanAnchorage/js/main.js
@@ -3,6 +3,10 @@
 	const getTemp = async (lat, long) => {
 		const API_URL = 'https://api.darksky.net/forecast/a895a7c5256b7eadc3074f3485db9406/';
 		const response = await fetch(`${API_URL}${lat},${long}?lang=en&units=us&exclude=minutely,hourly,daily,alerts,flags`);
+		if (!response.ok) {
+			setElementContent('well-is-it', '¯\\_(ツ)_/¯');
+			setElementContent('loading', 'Error getting temperatures');
+		}
 		const json = await response.json();
 
 		return parseFloat(json.currently.temperature);
@@ -18,6 +22,8 @@
 
 	const chicagoTemp = await getTemp(41.8369, -87.6847);
 	const anchorageTemp = await getTemp(61.2175, -149.8584);
+
+	toggleElementVisible('loading', false);
 
 	setElementContent('well-is-it', anchorageTemp > chicagoTemp ? 'YES' : 'NO');
 	setElementContent('chicagoTemp', chicagoTemp);
