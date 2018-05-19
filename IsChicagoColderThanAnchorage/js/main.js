449464b99d5d49436383faa1780f8da223666af6
Jim Segal <jsegal205@gmail.com>
2018-05-19 16:14:51 -0500

    add flex to footer, make error smaller on mobile

--- IsChicagoColderThanAnchorage/js/main.js
+++ IsChicagoColderThanAnchorage/js/main.js
@@ -4,6 +4,7 @@
 		const API_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=9bf0ee26559265f94c44015dbce8177d&units=imperial';
 		const response = await fetch(`${API_URL}&lat=${lat}&lon=${long}`);
 		if (!response.ok) {
+			getElementById('well-is-it').className = 'error';
 			setElementContent('well-is-it', '¯\\_(ツ)_/¯');
 			setElementContent('loading', 'Error getting temperatures');
 			return false;
@@ -13,18 +14,23 @@
 		return parseFloat(json.main.temp);
 	};
 
+	const getElementById = (eleId) => {
+		return document.getElementById(eleId);
+	}
+
 	const setElementContent = (eleId, content) => {
-		document.getElementById(eleId).innerHTML = content;
+		getElementById(eleId).innerHTML = content;
 	};
 
 	const toggleElementVisible = (eleId, isVisible) => {
-		document.getElementById(eleId).hidden = !isVisible;
+		getElementById(eleId).hidden = !isVisible;
 	};
 
 	const chicagoTemp = await getTemp(41.8369, -87.6847);
 	const anchorageTemp = await getTemp(61.2175, -149.8584);
 
 	if (chicagoTemp && anchorageTemp) {
+		getElementById('well-is-it').className = '';
 		toggleElementVisible('loading', false);
 
 		setElementContent('well-is-it', anchorageTemp > chicagoTemp ? 'YES' : 'NO');
