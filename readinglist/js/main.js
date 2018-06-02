168fafe1c64236aa80751f38f71641f8d85feb9e
Jim Segal <jsegal205@gmail.com>
2018-06-02 10:37:40 -0500

    add series section

--- readinglist/js/main.js
+++ readinglist/js/main.js
@@ -1,11 +1,43 @@
 import books from "./data/books.js";
 
 (() => {
-  const listEle = document.getElementById("book-list");
+  const bookListEle = document.getElementById("book-list");
+  const seriesListEle = document.getElementById("series-list");
+
+  let series = [];
   books.forEach(year => {
-    const container = document.createElement("section");
+    Object.values(year)[0].forEach(
+      yearBooks => (yearBooks.hasOwnProperty("series") ? series.push(yearBooks.series) : null)
+    );
+  });
+
+  const uniqSeries = series.filter(
+    (series, index, self) => self.findIndex(t => t.title === series.title && t.link === series.link) === index
+  );
+
+  uniqSeries
+    .sort((a, b) => {
+      const titleA = a.title.toUpperCase();
+      const titleB = b.title.toUpperCase();
+      if (titleA < titleB) {
+        return -1;
+      }
+      if (titleA > titleB) {
+        return 1;
+      }
+      return 0;
+    })
+    .forEach(series => {
+      const seriesItem = document.createElement("li");
+      seriesItem.className = "book";
+      seriesItem.innerHTML = anchorTmpl(series.link, series.title);
+      seriesListEle.appendChild(seriesItem);
+    });
+
+  books.forEach(year => {
+    const container = document.createElement("article");
     container.innerHTML = yearTmpl(year);
-    listEle.appendChild(container);
+    bookListEle.appendChild(container);
   });
 
   function mapBookType(type) {
