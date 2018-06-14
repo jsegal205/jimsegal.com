eb212990a314ad520cd6dc1ff3dd6a813c074e80
Jim Segal <jsegal205@gmail.com>
2018-06-13 20:46:17 -0500

    simplify series buildout

--- readinglist/js/main.js
+++ readinglist/js/main.js
@@ -1,38 +1,17 @@
 import books from "./data/books.js";
+import * as allSeries from "./data/series.js";
 
 (() => {
   const bookListEle = document.getElementById("book-list");
   const seriesListEle = document.getElementById("series-list");
 
-  let series = [];
-  books.forEach(year => {
-    Object.values(year)[0].forEach(
-      yearBooks => (yearBooks.hasOwnProperty("series") ? series.push(yearBooks.series) : null)
-    );
+  Object.keys(allSeries).forEach(key => {
+    const series = allSeries[key];
+    const seriesItem = document.createElement("li");
+    seriesItem.className = "book";
+    seriesItem.innerHTML = anchorTmpl(series.link, series.title);
+    seriesListEle.appendChild(seriesItem);
   });
-
-  const uniqSeries = series.filter(
-    (series, index, self) => self.findIndex(t => t.title === series.title && t.link === series.link) === index
-  );
-
-  uniqSeries
-    .sort((a, b) => {
-      const titleA = a.title.toUpperCase();
-      const titleB = b.title.toUpperCase();
-      if (titleA < titleB) {
-        return -1;
-      }
-      if (titleA > titleB) {
-        return 1;
-      }
-      return 0;
-    })
-    .forEach(series => {
-      const seriesItem = document.createElement("li");
-      seriesItem.className = "book";
-      seriesItem.innerHTML = anchorTmpl(series.link, series.title);
-      seriesListEle.appendChild(seriesItem);
-    });
 
   books.forEach(year => {
     const container = document.createElement("article");
