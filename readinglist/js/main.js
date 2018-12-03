14c0d9dfe31343c5c0bf1461d8abb96549afc665
Jim Segal <jsegal205@gmail.com>
2018-12-02 20:15:50 -0600

    fat arrow all the things

--- readinglist/js/main.js
+++ readinglist/js/main.js
@@ -5,6 +5,54 @@ import * as allSeries from "./data/series.js";
   const bookListEle = document.getElementById("book-list");
   const seriesListEle = document.getElementById("series-list");
 
+  const anchorTmpl = (link, desc) => {
+    return `<a href="${link}" target="_blank" rel="noopener">${desc}</a>`;
+  };
+
+  const mapBookType = type => {
+    switch (type) {
+      case "headphones":
+        return "audiobook";
+      default:
+        return type;
+    }
+  };
+
+  const yearTmpl = year => {
+    const books = Object.values(year)[0];
+    return `
+    <h2>${Object.keys(year)[0]}</h2>
+    <h3>Books read: ${books.length}</h3>
+    <ul>
+    ${bookTmpl(books)}
+    </ul>
+    `;
+  };
+
+  const bookTmpl = books => {
+    return books
+      .map(book => {
+        return `
+      <li class="book">
+      ${anchorTmpl(book.link, book.title)}
+      ${book.type ? `<i class="fa fa-${book.type}" title="${mapBookType(book.type)}"></i>` : ""}
+      <span> by ${book.author}</span>
+      ${book.series ? seriesTmpl(book.series) : ""}
+      </li>
+      `;
+      })
+      .join("");
+  };
+
+  const seriesTmpl = series => {
+    return `
+    <div class="series">
+    <span>Book ${series.bookNum} of </span>
+    ${anchorTmpl(series.link, series.title)}
+    </div>
+    `;
+  };
+
   Object.keys(allSeries).forEach(key => {
     const series = allSeries[key];
     const seriesItem = document.createElement("li");
@@ -18,52 +66,4 @@ import * as allSeries from "./data/series.js";
     container.innerHTML = yearTmpl(year);
     bookListEle.appendChild(container);
   });
-
-  function mapBookType(type) {
-    switch (type) {
-      case "headphones":
-        return "audiobook";
-      default:
-        return type;
-    }
-  }
-
-  function anchorTmpl(link, desc) {
-    return `<a href="${link}" target="_blank" rel="noopener">${desc}</a>`;
-  }
-
-  function yearTmpl(year) {
-    const books = Object.values(year)[0];
-    return `
-      <h2>${Object.keys(year)[0]}</h2>
-      <h3>Books read: ${books.length}</h3>
-      <ul>
-        ${bookTmpl(books)}
-      </ul>
-    `;
-  }
-
-  function bookTmpl(books) {
-    return books
-      .map(book => {
-        return `
-        <li class="book">
-          ${anchorTmpl(book.link, book.title)}
-          ${book.type ? `<i class="fa fa-${book.type}" title="${mapBookType(book.type)}"></i>` : ""}
-          <span> by ${book.author}</span>
-          ${book.series ? seriesTmpl(book.series) : ""}
-        </li>
-      `;
-      })
-      .join("");
-  }
-
-  function seriesTmpl(series) {
-    return `
-      <div class="series">
-        <span>Book ${series.bookNum} of </span>
-        ${anchorTmpl(series.link, series.title)}
-      </div>
-    `;
-  }
 })();
