92f95329427897b054aea14f5a4fb23974ed0af4
Jim Segal <jsegal205@gmail.com>
2019-01-12 07:38:04 -0500

    add hell divers ghosts

--- readinglist/js/data/2019.js
+++ readinglist/js/data/2019.js
@@ -2,6 +2,18 @@ import { hellDivers } from "./series.js";
 
 const nineteen = [
   {
+    title: "Ghosts",
+    author: "Nicholas Sansbury Smith",
+    link: "https://www.audible.com/pd/Hell-Divers-II-Ghosts-Audiobook/B01N5US9UJ",
+    type: "headphones",
+    duration: { hours: 8, minutes: 13 },
+    narrator: "R. C. Bray",
+    series: {
+      ...hellDivers,
+      bookNum: 2
+    }
+  },
+  {
     title: "Hell Divers",
     author: "Nicholas Sansbury Smith",
     link: "https://www.audible.com/pd/Hell-Divers-Audiobook/B01E97TPPA",
