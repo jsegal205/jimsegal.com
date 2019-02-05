230eff03bedc129e289c7ec1ddc4d64df6a9a227
Jim Segal <jsegal205@gmail.com>
2019-02-04 19:10:15 -0600

    add hell divers deliverance

--- readinglist/js/data/2019.js
+++ readinglist/js/data/2019.js
@@ -2,6 +2,18 @@ import { hellDivers } from "./series.js";
 
 const nineteen = [
   {
+    title: "Deliverance",
+    author: "Nicholas Sansbury Smith",
+    link: "https://www.audible.com/pd/Hell-Divers-III-Deliverance-Audiobook/B079NS2WGZ",
+    type: "headphones",
+    duration: { hours: 11, minutes: 18 },
+    narrator: "R. C. Bray",
+    series: {
+      ...hellDivers,
+      bookNum: 3
+    }
+  },
+  {
     title: "Ghosts",
     author: "Nicholas Sansbury Smith",
     link: "https://www.audible.com/pd/Hell-Divers-II-Ghosts-Audiobook/B01N5US9UJ",
