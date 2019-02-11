ae3f6a9b65b4874a1f9a94d6cc34fb24dc19d6eb
Jim Segal <jsegal205@gmail.com>
2019-02-10 22:07:47 -0600

    add local temp checking based on geolocation api

--- IsChicagoColderThanAnchorage/js/main.js
+++ IsChicagoColderThanAnchorage/js/main.js
@@ -1,45 +1,55 @@
+const getTemp = async (lat, long) => {
+  const API_URL =
+    "http://api.openweathermap.org/data/2.5/weather?appid=9bf0ee26559265f94c44015dbce8177d&units=imperial";
+  const response = await fetch(`${API_URL}&lat=${lat}&lon=${long}`);
+  if (!response.ok) {
+    getElementById("well-is-it").className = "error";
+    setElementContent("well-is-it", "¯\\_(ツ)_/¯");
+    setElementContent("loading", "Error getting temperatures");
+    return false;
+  }
+  const json = await response.json();
+
+  return parseFloat(json.main.temp);
+};
+
+const getElementById = eleId => {
+  return document.getElementById(eleId);
+};
+
+const setElementContent = (eleId, content) => {
+  getElementById(eleId).innerHTML = content;
+};
+
+const toggleElementVisible = (eleId, isVisible) => {
+  getElementById(eleId).hidden = !isVisible;
+};
+
+const verdictText = (anchorageTemp, compareTemp) => {
+  if (anchorageTemp > compareTemp) {
+    return "YUP";
+  }
+
+  if (anchorageTemp > compareTemp - 5) {
+    return "ALMOST";
+  }
+
+  return "NOPE";
+};
+
+let anchorageTemp;
+
+// page load
 (async () => {
-  const getTemp = async (lat, long) => {
-    const API_URL =
-      "http://api.openweathermap.org/data/2.5/weather?appid=9bf0ee26559265f94c44015dbce8177d&units=imperial";
-    const response = await fetch(`${API_URL}&lat=${lat}&lon=${long}`);
-    if (!response.ok) {
-      getElementById("well-is-it").className = "error";
-      setElementContent("well-is-it", "¯\\_(ツ)_/¯");
-      setElementContent("loading", "Error getting temperatures");
-      return false;
-    }
-    const json = await response.json();
-
-    return parseFloat(json.main.temp);
-  };
-
-  const getElementById = eleId => {
-    return document.getElementById(eleId);
-  };
-
-  const setElementContent = (eleId, content) => {
-    getElementById(eleId).innerHTML = content;
-  };
-
-  const toggleElementVisible = (eleId, isVisible) => {
-    getElementById(eleId).hidden = !isVisible;
-  };
-
-  const verdictText = (anchorageTemp, chicagoTemp) => {
-    if (anchorageTemp > chicagoTemp) {
-      return "YUP";
-    }
-
-    if (anchorageTemp > chicagoTemp - 5) {
-      return "ALMOST";
-    }
-
-    return "NOPE";
-  };
+  if ("geolocation" in navigator) {
+    const localCompare = getElementById("check-my-location");
+    localCompare.onclick = browserLocationCompare;
+  } else {
+    toggleElementVisible("browser-location", false);
+  }
 
   const chicagoTemp = await getTemp(41.8369, -87.6847);
-  const anchorageTemp = await getTemp(61.2175, -149.8584);
+  anchorageTemp = await getTemp(61.2175, -149.8584);
 
   if (chicagoTemp && anchorageTemp) {
     getElementById("well-is-it").className = "";
@@ -53,3 +63,24 @@
     toggleElementVisible("anchorage", true);
   }
 })();
+
+function browserLocationCompare() {
+  const success = async position => {
+    const browserLat = position.coords.latitude.toFixed(6);
+    const browserLong = position.coords.longitude.toFixed(6);
+    const locationTemp = await getTemp(browserLat, browserLong);
+
+    setElementContent("location-header", `Is ${browserLat}, ${browserLong} colder than Anchorage??`);
+    setElementContent("location-is-it", verdictText(anchorageTemp, locationTemp));
+    setElementContent("location-temp", locationTemp);
+    toggleElementVisible("location", true);
+  };
+
+  const error = () => {
+    setElementContent("location-header", "Unable to retrieve your location");
+  };
+
+  setElementContent("location-header", "Checking...");
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
