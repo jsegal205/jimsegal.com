fe839df52ea7ab6f0148d4d9a9c2a08d5e7f1db2
Jim Segal <hello@jimsegal.com>
2020-01-05 11:12:09 -0600

    add error handling to the async call

--- tabletop/js/main.js
+++ tabletop/js/main.js
@@ -1,15 +1,26 @@
 (async () => {
+  const gameCardsEle = document.getElementById("game-cards");
+
   const response = await fetch(
-    "https://data.heroku.com/dataclips/donygkplrgieljfwbfisudzjmirb.json"
-  );
+    "https://data.heroku.com/dataclips/donygkplrgieljfwbfisudzjmirbZ.json"
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Error getting games list.");
+      }
+      return response;
+    })
+    .catch(error => {
+      gameCardsEle.innerHTML = "Error getting games list.";
+      console.log(error);
+    });
+
   const gamesJson = await response.json();
   const games = gamesJson.values.map(game => {
     return { title: game[0], link: game[1], image: game[2] };
   });
 
   document.getElementById("game-count").innerText = games.length;
-
-  const gameCardsEle = document.getElementById("game-cards");
 
   const gameCardTemplate = game => {
     return `<a class="card" href="${game.link}" target="_blank" rel="noopener">
