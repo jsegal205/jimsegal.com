9458d14ea13e7bcc59a81cac963dad399772180e
Jim Segal <hello@jimsegal.com>
2020-01-10 20:33:42 -0600

    sethmode

--- js/main.js
+++ js/main.js
@@ -29,3 +29,36 @@ const setDarkMode = shouldBeDark => {
 };
 
 setDarkMode(getDarkModeCookie());
+
+const keyMap = {
+  83: "s",
+  69: "e",
+  84: "t",
+  72: "h",
+  77: "m",
+  79: "o",
+  68: "d"
+};
+let sequenceTracker = 0;
+const sethmodeSequence = ["s", "e", "t", "h", "m", "o", "d", "e"];
+
+const toggleSethMode = () => {
+  const sethEle = document.getElementsByClassName("seth")[0];
+  sethEle.className = `seth${
+    sethEle.classList.contains("hidden") ? "" : " hidden"
+  }`;
+};
+
+document.addEventListener("keydown", e => {
+  const key = keyMap[e.keyCode];
+  const requiredKey = sethmodeSequence[sequenceTracker];
+  if (key === requiredKey) {
+    sequenceTracker++;
+    if (sequenceTracker === sethmodeSequence.length) {
+      toggleSethMode();
+      sequenceTracker = 0;
+    }
+  } else {
+    sequenceTracker = 0;
+  }
+});
