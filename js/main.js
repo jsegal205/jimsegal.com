407b74134ba2253893e22c366efd1b146340736c
Jim Segal <hello@jimsegal.com>
2020-01-17 20:48:38 -0600

    extract dark mode logic to separate file

--- js/main.js
+++ js/main.js
@@ -1,39 +1,4 @@
-const chk = document.getElementById("toggle-chk");
-const body = document.getElementsByTagName("body")[0];
-const COOKIENAME = "jimsegaldarkmode";
 const SETHMODEELE = "seth-bg";
-
-const toggleDarkMode = e => {
-  const { checked } = e.target;
-  gtag("event", "toggled", {
-    event_category: "darkmode",
-    event_label: checked
-  });
-  setDarkModeCookie(checked);
-  setDarkMode(checked);
-};
-
-chk.addEventListener("change", toggleDarkMode, false);
-
-const getDarkModeCookie = () => {
-  const darkModeValue = document.cookie.match(
-    `(^|;) ?${COOKIENAME}=([^;]*)(;|$)`
-  );
-  return darkModeValue ? darkModeValue[2].toLowerCase() === "true" : null;
-};
-
-const setDarkModeCookie = value => {
-  const d = new Date();
-  d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * 365);
-  document.cookie = `${COOKIENAME}=${value};path=/;expires=${d.toGMTString()}`;
-};
-
-const setDarkMode = shouldBeDark => {
-  chk.checked = shouldBeDark;
-  body.className = shouldBeDark ? "dark" : "";
-};
-
-setDarkMode(getDarkModeCookie());
 
 const keyMap = {
   68: "d",
