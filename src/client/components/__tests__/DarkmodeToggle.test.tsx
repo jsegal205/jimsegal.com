096ddb119cbdce01fedc52a8f2ac2863685ade13
Jim Segal <hello@jimsegal.com>
2021-06-08 22:12:19 -0500

    create darkmode component and context

---
+++ src/client/components/__tests__/DarkmodeToggle.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import DarkModeToggle from "../DarkmodeToggle";
+import { DarkmodeProvider } from "../../contexts/DarkmodeContext";
+
+describe("<DarkmodeToggle />", () => {
+  beforeEach(() => {
+    render(
+      <DarkmodeProvider>
+        <DarkModeToggle />
+      </DarkmodeProvider>
+    );
+  });
+
+  describe("when component is initialized", () => {
+    it("then sets the light class by default", () => {
+      expect(screen.getByText(/Dark/i)).toBeTruthy();
+      expect(document.body.className).toEqual("");
+    });
+  });
+
+  describe("when the toggle theme input is clicked", () => {
+    beforeEach(() => {
+      userEvent.click(screen.getByText(/Dark/i));
+    });
+
+    it("then sets the dark class", () => {
+      expect(screen.getByText(/Light/i)).toBeTruthy();
+      expect(document.body.className).toEqual("dark");
+    });
+  });
+});
